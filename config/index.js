0d0e7f799a321144e14ab0936c4fb37935eec520
nightblood <nightblood@localhost.localdomain>
2021-07-09 12:33:29 +0100

    work

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    MONGO_URI: process.env.MONGODB_URI,
+    MONGO_URI: process.env.MONGO_URI,
     SUPERVISOR_JWT: process.env.SUPERVISOR_JWT,
     USER_JWT: process.env.USER_JWT,
 }
